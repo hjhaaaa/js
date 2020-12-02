286375f2c5b9a9d0812a870139cb10518d58af93
Wuzy <Wuzy@HZZ-PC>
2020-12-02 09:18:06 +0800

    优化docker打包bat

--- nyfd-web-docker-build-dev.bat
+++ nyfd-web-docker-build-dev.bat
@@ -2,3 +2,4 @@
 docker rm registry.cn-hangzhou.aliyuncs.com/mgtest/nyfd-web-dev
 docker rmi registry.cn-hangzhou.aliyuncs.com/mgtest/nyfd-web-dev 
 docker build -f docker/DockerfileDev -t registry.cn-hangzhou.aliyuncs.com/mgtest/nyfd-web-dev --no-cache .
+docker push registry.cn-hangzhou.aliyuncs.com/mgtest/nyfd-web-dev 
--- nyfd-web-docker-build-pro.bat
+++ nyfd-web-docker-build-pro.bat
@@ -2,3 +2,4 @@
 docker rm registry.cn-hangzhou.aliyuncs.com/mgtest/nyfd-web-pro
 docker rmi registry.cn-hangzhou.aliyuncs.com/mgtest/nyfd-web-pro
 docker build -f docker/DockerfileRro -t registry.cn-hangzhou.aliyuncs.com/mgtest/nyfd-web-pro --no-cache .
+docker push registry.cn-hangzhou.aliyuncs.com/mgtest/nyfd-web-pro
